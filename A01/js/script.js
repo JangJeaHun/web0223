6f63775b8d23eaaf42e7f129bfc250404c72dcb3
JangJeaHun <wkdwogns2345@naver.com>
2022-02-23 16:40:40 +0900

    02 23

---
+++ A01/js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+  hover();
+  slide();
+  popup();
+    tab();
+});
+
+function slide() {
+  var idx = 0;
+  setInterval(function () {
+    $("#slide>a")
+      .eq(idx)
+      .animate(
+        {
+          top: "-100%",
+        },
+        400
+      )
+      .animate({ top: "100%" }, 0);
+
+    if (idx < 2) {
+      idx++;
+    } else {
+      idx = 0;
+    }
+
+    $("#slide>a").eq(idx).animate(
+      {
+        top: "0%",
+      },
+      400
+    );
+  }, 3000);
+}
+
+function popup() {
+  $(".pop").click(function () {
+    $("#popup").fadeIn();
+  });
+
+  $(".close").click(function () {
+    $("#popup").hide();
+  });
+}
+
+
+function hover() {
+  $("#gnb>li").hover(
+    function () {
+      $(".submenu", this).stop().slideDown();
+    },
+    function () {
+      $(".submenu", this).stop().slideUp();
+    }
+  );
+}
+
+function tab(){
+    $("#tab>li").click(function(){
+        var idx = $(this).index();
+        $("#recent>div").eq(idx).show()  .siblings('div').hide();
+        $(this).addClass('selected')  .siblings('li').removeClass('selected');
+        // $(this).siblings('li').removeClass('selected');
+
+
+        
+    });
+}
