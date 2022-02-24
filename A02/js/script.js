e0b51dba2785048dfe7aa50a953afba6ae4ebef7
JangJeaHun <wkdwogns2345@naver.com>
2022-02-24 16:59:01 +0900

    0224

--- A02/js/script.js
+++ A02/js/script.js
@@ -10,9 +10,13 @@ $(document).ready(function(){
     });
 
     $('.pop').click(function(event){
-        event.preventDefault();
         $('#modal').fadeIn();
+        // event.preventDefault();
+        return false
     });
+
+    // event.preventDefault(); 이나  return false 사용가능.
+       
     $('.close').click(function(){
         $('#modal').fadeOut();
     });
