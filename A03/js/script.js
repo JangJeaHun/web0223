907079e0fef20800abc02edd443227ab206876b5
JangJeaHun <wkdwogns2345@naver.com>
2022-02-28 16:49:58 +0900

    0228

--- A03/js/script.js
+++ A03/js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
 
-    
+    var idx=0;
+
+    setInterval(function(){
+        $("#silde>a").eq(idx).fadeOut(400);
+
+        if(idx<2){
+            idx++;
+        }else{
+            idx=0;
+        }
+
+        $("#silde>a").eq(idx).fadeIn(400);
+
+    },3000);
 
 
 
