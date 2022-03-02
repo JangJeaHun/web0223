eabfd1710c40894a36e4b28dd314c19f9889229a
JangJeaHun <wkdwogns2345@naver.com>
2022-03-02 14:32:11 +0900

    0302

--- A03/js/script.js
+++ A03/js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+hover();
+slide();
+tab();
+popup();
+});
 
+
+function hover(){
+    $("#gnb>li").hover(function(){
+        $(".submenu",this).stop().slideDown();
+    },function(){
+        $(".submenu",this).stop().slideUp();
+    })
+}
+
+function slide(){
     var idx=0;
-
     setInterval(function(){
         $("#slide>a").eq(idx).fadeOut(300);
 
@@ -14,7 +28,24 @@ $(document).ready(function(){
         $("#slide>a").eq(idx).fadeIn(300);
 
     },3000);
+}
 
 
+function tab(){
+    $("#tab>li").click(function(){
 
-});
+        var idx = $(this).index();
+        $("#content>div").eq(idx).show().siblings("div").hide();
+        $(this).addClass("selected").siblings("li").removeClass();
+
+
+    })
+}
+function popup(){
+    $(".pop").click(function(){
+        $("#popup").fadeIn();
+    })
+    $(".close").click(function(){
+        $("#popup").fadeOut();
+    })
+}
