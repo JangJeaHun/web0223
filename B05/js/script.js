de1718ba39e7873931ac672bb96cad50b0e6a202
JangJeaHun <wkdwogns2345@naver.com>
2022-03-07 16:21:59 +0900

    0307 B07

--- B05/js/script.js
+++ B05/js/script.js
@@ -4,7 +4,62 @@ menu();
 slide();
 tab();
 popup();
+
+var map = new naver.maps.Map('map', {
+    center: new naver.maps.LatLng(35.15357071627816, 126.88799732174014),
+    zoom: 18
+});
+
+var marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(35.15357071627816, 126.88799732174014),
+    map: map
+});
+
+var btns = $(".buttons > input");
+btns.on("click", function(e) {
+    e.preventDefault();
+
+    var mapTypeId = this.id;
+
+    if (map.getMapTypeId() !== naver.maps.MapTypeId[mapTypeId]) {
+        map.setMapTypeId(naver.maps.MapTypeId[mapTypeId]); // 지도 유형 변경하기
+
+        btns.removeClass("control-on");
+        $(this).addClass("control-on");
+    }
+});
+
+
+$("#chkPop").change(function(){
+    setCookie("pop","close",3);
 })
+
+getCookie();
+if(getCookie("pop") =="close"){
+    $("#popup").hide();
+};
+
+
+
+function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; path=/; expires=" + date.toUTCString();
+};
+
+function getCookie(name) {
+
+    var cookie = document.cookie;
+    if (document.cookie != "") {
+         var cookieArray = cookie.split("; ");
+         for ( var index in cookieArray) {
+             var cookieName = cookieArray[index].split("=");
+             if (cookieName[0] == name) {
+                  return cookieName[1];
+             }
+         }
+    } return ;    
+};
 
 function menu(){
     $("#gnb>li").hover(function(){
@@ -12,7 +67,7 @@ function menu(){
     },function(){
         $(".submenu").stop().slideToggle();
     })
-}
+};
 
 function slide(){
     var idx=0;
@@ -33,7 +88,7 @@ function slide(){
             left:"0"
         },400);
     },3000);
-}
+};
 function tab(){
     $("#tab>li").click(function(){
         var idx=$(this).index();
@@ -49,4 +104,6 @@ function popup(){
     $(".close").click(function(){
         $("#popup").hide();
     })
-}
+};
+
+})
