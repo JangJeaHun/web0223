01ac04187f26e1be1301559617fe1eca6653ec20
J <wkdwogns2345@naver.com>
2022-03-17 19:36:17 +0900

    B06 수정

--- B06/js/script.js
+++ B06/js/script.js
@@ -1,45 +1,38 @@
-$(document).ready(function () {
-  $("#gnb>li, #mBack").hover(
-    function () {
-      $(".submenu").stop().slideDown();
-      $("#mBack").stop().slideDown();
-    },
-    function () {
-      $(".submenu").stop().slideUp();
-      $("#mBack").stop().slideUp();
-    }
-  );
+$(document).ready(function(){
+  $("#gnb>li").hover(function(){
+    $(".submenu").stop().slideDown();
+    $("#mBack").stop().slideDown();
+  },function(){
+    $(".submenu").stop().slideUp();
+    $("#mBack").stop().slideUp();
+  })
 
-  var idx = 0;
-  setInterval(function () {
-    $("#slide>a")
-      .eq(idx)
-      .stop()
-      .animate({
-        top: "-100%",
-      })
-      .animate({ top: "100%" }, 0);
-
-    if (idx < 2) {
+  idx=0;
+  setInterval(function(){
+    $("#slide>a").eq(idx).animate({
+      top:"-100%"
+    }).animate({
+      top:"100%"
+    },0)
+    
+    if(idx<2){
       idx++;
-    } else {
-      idx = 0;
+    }else{
+      idx=0;
     }
+    $("#slide>a").eq(idx).animate({
+      top:0
+    })
+  },2000)
 
-    $("#slide>a").eq(idx).stop().animate({
-      top: 0,
-    });
-  }, 2000);
-
-  $("#url").change(function(){
-    open($(this).val());
-})
-
-
-  $(".pop").click(function () {
+  $(".pop").click(function(){
     $("#modal").fadeIn();
-  });
-  $(".close").click(function () {
+  })
+  $(".close").click(function(){
     $("#modal").hide();
-  });
-});
+  })
+
+
+  
+
+})
