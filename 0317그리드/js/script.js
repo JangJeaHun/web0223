49b12e6bfcd70a1b1b5e2e2ef7e5bc15b7df82a9
JangJeaHun <wkdwogns2345@naver.com>
2022-03-18 16:54:22 +0900

    0318

--- 0317그리드/js/script.js
+++ 0317그리드/js/script.js
@@ -1,9 +1,73 @@
 $(document).ready(function(){
     $("#mainSlide").mSlide(5, false);
     $("#miniSlide").mSlide(3, true);
-    
+
+    let wheelOff = false;
+
+    $("#mainSlide").on("wheel",function(e){
+        let dir = e.originalEvent.wheelDelta;
+        if(dir<0 && !wheelOff){
+            $(document).scrollTop($(window).height());
+            wheelOff = true;
+        }
+        if(dir>0 && !wheelOff){
+            $(document).scrollTop($(window).height(0));
+            wheelOff = true;
+        }
+        // alert(dir);
+    });
+    setInterval(function(){
+        wheelOff = false;
+    },500)
+
+    $("#innerNew").itemSlide(3);
 
 });
+$.fn.itemSlide = function(interval){
+    let obj = $(this);
+    let lastX, is;
+    let imgW = $("a>img",this).eq(0).width() + 10;
+
+
+
+    $("a",this).each(function(index){
+        $(this).css("left",index*imgW);
+        lastX= index*imgW;
+    });
+
+    if($(window).width() >= 768){
+        iSlide();
+    };
+
+    $("#newArrival").on("touchstart",function(e){
+        clearInterval(is);
+        startX = e.originalEvent.touches[0].pageX;
+
+        
+    });
+
+    function iSlide(){
+        is = setInterval(function(){
+            $("a",obj).each(function(){
+                $(this).animate({
+                    left:"-=" + imgW 
+                })
+            let leftX = parseInt($(this).css("left"));
+            if(leftX <= 0){
+                $(this).animate({left:lastX},0);
+            }
+            });
+        },interval*1000)
+    }
+
+
+
+    
+    
+};
+
+
+
 
 $.fn.mSlide = function(interval){
     let idx=0;
@@ -25,7 +89,7 @@ $.fn.mSlide = function(interval){
             $("a>h2",obj).stop(false,true).eq(idx).slideUp();
             $("a>p",obj).stop(false,true).eq(idx).fadeOut();
 
-            if(idx<maxIdx){h
+            if(idx<maxIdx){
                 idx++;
             }else{
                 idx=0;
@@ -63,9 +127,9 @@ $.fn.mSlide = function(interval){
             $('a',obj).eq(idx).delay(800).animate({left:"-100%"}).animate({left:"100%"},0);
             $(".indi>li",obj).eq(idx).removeClass("current");
 
-            if(!stopText){
+            // if(!stopText){
                 
-            }
+            // }
             $("a>h2",obj).eq(idx).slideUp();
             $("a>p",obj).eq(idx).delay(400).fadeOut();
             
