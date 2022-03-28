dc2ede58c4a5cdb17f5260be984340faf67f90c3
JangJeaHun <wkdwogns2345@naver.com>
2022-03-28 16:17:17 +0900

    0328 B06RE finish

---
+++ B06Re/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $("#gnb>li, #mBack").hover(function(){
+    $(".submenu,#mBack").stop().slideToggle();
+  },function(){
+    $(".submenu,#mBack").stop().slideToggle();
+  })
+
+  var idx = 0;
+  setInterval(function(){
+    $("#slide>a").eq(idx).stop().animate({top:"-100%"}).animate({top:"100%"},0)
+
+    if(idx<2){
+      idx++;
+    }else{
+      idx=0;
+    }
+    $("#slide>a").eq(idx).stop().animate({top:0})
+
+  },3000)
+
+  $(".pop").click(function(){
+    $("#modal").show();
+  })
+  $(".close").click(function(){
+    $("#modal").hide();
+  })
+
+
+  $("#url").change(function(){
+    var url = $(this).val();
+    window.open(url);
+  })
+
+
+})
