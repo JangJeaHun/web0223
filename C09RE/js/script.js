18e37a7c40e4966b06c10fef072609137f7bfd67
JangJeaHun <wkdwogns2345@naver.com>
2022-03-28 14:49:17 +0900

    0328 09RE finish

---
+++ C09RE/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#gnb>li").hover(function(){
+        $(".submenu",this).stop().slideToggle();
+    },function(){
+        $(".submenu",this).stop().slideToggle();
+    })
+
+    var idx = 0;
+    setInterval(function(){
+        $("#slide>a").eq(idx).stop().fadeOut();
+
+        if(idx<2){
+            idx++;
+        }else{
+            idx=0;
+        }
+        
+        $("#slide>a").eq(idx).stop().fadeIn();
+    },3000)
+
+    $("#tab>li").click(function(){
+        var idx = $(this).index();
+
+        $(this).addClass("selected").siblings("li").removeClass();
+        $("#recent>div").eq(idx).show().siblings("div").hide();
+    })
+
+
+    $(".pop").click(function(){
+        $("#popup").show();
+    })
+    $(".close").click(function(){
+        $("#popup").hide();
+    })
+
+})
